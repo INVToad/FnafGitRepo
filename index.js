8c8b8d71d12d83c9cd3e0611c23ac29a598051b4
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-16 04:13:10 +0000

    more

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+import { io } from 'socket.io-client'
+
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = require('socket.io')(3000)
+
+const socket = io()
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
