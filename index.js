120739fac6b3b9214a23f099d074b5b8e83008dc
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-16 04:09:49 +0000

    IO test

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require('socket.io')(3000)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('a user connected');
 });
 
