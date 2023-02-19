7e3edc639b01c3754c19557436b8d41c7387bbb7
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-19 23:13:53 +0000

    Rid of extra functions

--- index.js
+++ index.js
@@ -19,76 +19,10 @@ var playerPos = {}
 
 io.on('connection', async (socket) => {
   io.sockets.emit("connected")
-  socket.on('playerJoin', (data) => {
-    playerPos[data.id] = {
-      x: data.x,
-      y: data.y,
-      velX: data.velX,
-      velY: data.velY,
-      angle: data.angle,
-      angVel: data.angVel,
-      scale: data.scale,
-      username: data.username,
-      browserId: data.browserId
-    }
-    socket.emit("createExistingPlayers", playerPos)
-    io.sockets.emit("createPlayer", data)
-    socket.emit("askCoords")
-  });
-
-  socket.on('coords', (data) => {
-    var timestamp = new Date().getTime();
-    playerPos[data.id] = {
-      x: data.x,
-      y: data.y,
-      velX: data.velX,
-      velY: data.velY,
-      angle: data.angle,
-      angVel: data.angVel,
-      scale: data.scale,
-      username: data.username,
-      browserId: data.browserId
-    }
-
-    socket.emit("updatePlayers", { playerdata: playerPos, ts: timestamp })
-  });
-
   socket.on('disconnect', function() {
     io.sockets.emit("disconnected")
-    try {
-      io.sockets.emit("receiveMessage", { username: playerPos[socket.id].username, type: "left", message: { time: 100 } })
-      io.sockets.emit("removePlayer", { id: socket.id, username: playerPos[socket.id].username })
-      delete playerPos[socket.id]
-    } catch (error) {
-      console.log(error)
-    }
-  })
 
-  socket.on('inactive', function(data) {
-    io.sockets.emit("receiveMessage", { username: data, type: "left", message: { time: 100 } })
-    io.sockets.emit("removePlayer", { id: socket.id, username: data })
-    delete playerPos[socket.id]
-    socket.disconnect(true)
   })
-
-  socket.on('kick', function(data) {
-    var hash = CryptoJS.SHA256(data.key).toString();
-    if (hash == hashedKey) {
-      io.to(data.id).emit('beenKicked', data.message);
-      io.sockets.sockets.forEach((socket) => {
-        if (socket.id == data.id) {
-          io.sockets.emit("removePlayer", { id: socket.id, username: playerPos[socket.id].username })
-          socket.disconnect(true);
-          delete playerPos[socket.id]
-        }
-      });
-    }
-  })
-
-  socket.on('sendMessage', function(data) {
-    io.sockets.emit("receiveMessage", data)
-  })
-
 })
 
 
