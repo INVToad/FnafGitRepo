8efed06bb9a1b86fff2a4df2d3cbd023fa3b0a4d
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-20 02:08:37 +0000

    making chat

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var CryptoJS = require("crypto-js");
 const server = http.createServer(app);
 var hashedKey = "244dc524b6bba33086418c1a68cb4bd95304a2562489c6c19d5c785979f48b7f";
 
+var playerUsernames = []
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "*"
@@ -15,13 +17,16 @@ app.get('/', (req, res) => {
   res.send('server');
 });
 
-var playerPos = {}
-
 io.on('connection', async (socket) => {
-  io.sockets.emit("connected")
+  socket.on('connect', function(user) {
+    playerUsernames.push(user)
+    io.sockets.emit("connected")
+  })
   socket.on('disconnect', function() {
     io.sockets.emit("disconnected")
-
+  })
+  socket.on('SendMsg', function(msg) {
+    io.sockets.emit('RetrieveMsg', msg)
   })
 })
 
