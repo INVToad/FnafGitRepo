14bef6c149a7d7cbd96daea62c18f3c13f4f1a5b
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-22 02:34:58 +0000

    fix

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ io.on('connection', async (socket) => {
       socket.emit("connected", userNames[arg2])
     }
   })
-  socket.on('disconnection', function(data) {
+  socket.on('disconnect', function() { })
+  socket.on('disconnected', function(data) {
     var e = CurrentUserNames.indexOf(userNames[data])
     CurrentUserNames.splice(e, e)
     socket.emit("disconnected", userNames[data])
