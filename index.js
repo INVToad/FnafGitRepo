c4bdcb5ab6ad57bac5e0782bab6cb7a7625d930a
Jareth green <123918584+INVToad@users.noreply.github.com>
2023-02-22 01:20:07 +0000

    hope

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ var userNames = {}
 var CurrentUserNames = []
 
 io.on('connection', async (socket) => {
-  socket.on('connect', function(arg1, arg2) {
-    if (CurrentUserNames.includes(arg1)){
+  socket.on('connect', function() {})
+  socket.on('connected', function(arg2, arg1) {
+    if (CurrentUserNames.includes(arg1)) {
       socket.emit('user', 'Taken')
     } else {
       CurrentUserNames.push(arg1)
